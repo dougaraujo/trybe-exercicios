b161a88ebd0cfbdb0ef4089e8066bb33918ff751
Douglas Araújo <goudxs@gmail.com>
2022-06-22 19:15:10 -0300

    Descobre quantos valores ímpares existem no array e imprime o resultado

--- fundamentos/introducao-js-logica-de-programacao/dia-2-array-e-loop-for/app.js
+++ fundamentos/introducao-js-logica-de-programacao/dia-2-array-e-loop-for/app.js
@@ -1,11 +1,13 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-let biggestNumber = numbers[0];
+let oddNumbers = [];
 
 for (let index = 0; index < numbers.length; index += 1) {
-    if (numbers[index] > biggestNumber) biggestNumber = numbers[index]; 
+    if ((numbers[index] % 2) != 0) {
+        oddNumbers.push(numbers[index]);
+    }
 }
 
-console.log(biggestNumber);
+console.log(oddNumbers.length);
 
 
