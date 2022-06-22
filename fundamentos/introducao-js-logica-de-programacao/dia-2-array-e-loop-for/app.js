571f3f9ad97348333d0192b0ff3ee290cfbb9b94
Douglas Araújo <goudxs@gmail.com>
2022-06-22 19:17:48 -0300

    Imprime na tela uma mensagem caso não haja valor ímpar contido no array numbers

--- fundamentos/introducao-js-logica-de-programacao/dia-2-array-e-loop-for/app.js
+++ fundamentos/introducao-js-logica-de-programacao/dia-2-array-e-loop-for/app.js
@@ -3,11 +3,13 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let oddNumbers = [];
 
 for (let index = 0; index < numbers.length; index += 1) {
-    if ((numbers[index] % 2) != 0) {
-        oddNumbers.push(numbers[index]);
-    }
+  if (numbers[index] % 2 != 0) {
+    oddNumbers.push(numbers[index]);
+  }
 }
 
-console.log(oddNumbers.length);
-
-
+if (console.log(oddNumbers.length) != 0) {
+  console.log(oddNumbers.length);
+} else {
+  console.log("nenhum valor ímpar encontrado");
+}
