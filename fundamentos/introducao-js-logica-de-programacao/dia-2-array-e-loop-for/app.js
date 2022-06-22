245b3a0ee76e088407adb2d72e12ac89fa047e13
Douglas Araújo <goudxs@gmail.com>
2022-06-22 18:28:41 -0300

    Descobre qual o maior valor contido no array e imprime-o

--- fundamentos/introducao-js-logica-de-programacao/dia-2-array-e-loop-for/app.js
+++ fundamentos/introducao-js-logica-de-programacao/dia-2-array-e-loop-for/app.js
@@ -1,16 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-let sum = 0;
+let biggestNumber = numbers[0];
 
 for (let index = 0; index < numbers.length; index += 1) {
-    sum += numbers[index];
+    if (numbers[index] > biggestNumber) biggestNumber = numbers[index]; 
 }
 
-let mediaAritmetica = sum / numbers.length;
+console.log(biggestNumber);
 
-if (mediaAritmetica <= 20) {
-    console.log('Valor menor ou igual a 20');
-} else {
-    console.log('Valor maior que 20');
-}
 
