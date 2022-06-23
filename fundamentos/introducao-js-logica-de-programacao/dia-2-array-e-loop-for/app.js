d813df212d3bb65ea14792c0c9c3711fdf75a9a2
Douglas Araújo <goudxs@gmail.com>
2022-06-23 09:59:44 -0300

    Cria um array que recebe cada elemento de numbers multiplicado pelo seu sucessor, o último elemento é multiplicado por 2

--- fundamentos/introducao-js-logica-de-programacao/dia-2-array-e-loop-for/app.js
+++ fundamentos/introducao-js-logica-de-programacao/dia-2-array-e-loop-for/app.js
@@ -1,13 +1,14 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (indexA = 1; indexA < numbers.length; indexA++) {
-  for (indexB = 0; indexB < indexA; indexB++) {
-    if (numbers[indexB] < numbers[indexA]) {
-      let swap = numbers[indexB];
-      numbers[indexB] = numbers[indexA];
-      numbers[indexA] = swap;
-    }
+let numbersMultiplied = [];
+
+for (index = 0; index < numbers.length; index++) {
+  if (index != numbers.length - 1) {
+    numbersMultiplied.push(numbers[index] * numbers[index + 1]);
+  } else {
+    numbersMultiplied.push(numbers[index] * 2);
   }
 }
 
-console.log(numbers);
+console.log(numbersMultiplied);
+ 
