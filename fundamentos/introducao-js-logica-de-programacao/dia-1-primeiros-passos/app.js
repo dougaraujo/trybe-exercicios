e1f02c4e0d58c8884ce7150c999b25f8c63ff16b
Douglas Araújo <goudxs@gmail.com>
2022-06-23 10:31:39 -0300

    Cria um programa para cada operação aritmética básica

---
+++ fundamentos/introducao-js-logica-de-programacao/dia-1-primeiros-passos/app.js
@@ -0,0 +1,11 @@
+const a = 15;
+
+const b = 58;
+
+let sum = a + b;
+let sub = a - b;
+let product = a * b;
+let division = a / b;
+let left = a % b;
+
+console.log(sum, sub, product, division,left);
