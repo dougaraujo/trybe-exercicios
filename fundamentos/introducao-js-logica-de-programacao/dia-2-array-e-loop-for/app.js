f4a6d0712dbd84a4a7e91d99e5dacd81fa20b4d7
Douglas Araújo <goudxs@gmail.com>
2022-06-23 09:50:05 -0300

    Ordena o array numbers em ordem decrescente e imprime seus valores

--- fundamentos/introducao-js-logica-de-programacao/dia-2-array-e-loop-for/app.js
+++ fundamentos/introducao-js-logica-de-programacao/dia-2-array-e-loop-for/app.js
@@ -2,7 +2,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 for (indexA = 1; indexA < numbers.length; indexA++) {
   for (indexB = 0; indexB < indexA; indexB++) {
-    if (numbers[indexB] > numbers[indexA]) {
+    if (numbers[indexB] < numbers[indexA]) {
       let swap = numbers[indexB];
       numbers[indexB] = numbers[indexA];
       numbers[indexA] = swap;
