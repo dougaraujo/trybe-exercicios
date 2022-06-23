6b0d678f705d88efe34ae54b9a06d51ad0b7b60b
Douglas Araújo <goudxs@gmail.com>
2022-06-23 08:59:47 -0300

    Cria um array que vai de 1 até 25 e imprime o resultado

--- fundamentos/introducao-js-logica-de-programacao/dia-2-array-e-loop-for/app.js
+++ fundamentos/introducao-js-logica-de-programacao/dia-2-array-e-loop-for/app.js
@@ -1,9 +1,7 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let output = [];
 
-let smallestNumber = numbers[0];
-
-for (let index = 0; index < numbers.length; index += 1) {
-    if (numbers[index] < smallestNumber) smallestNumber = numbers[index]; 
+for (let index = 1; index <= 25; index += 1) {
+  output.push(index);
 }
 
-console.log(smallestNumber);
+console.log(output);
