5ab37b1e9b431285f58c2555b387b726f5031ba4
Douglas Araújo <goudxs@gmail.com>
2022-06-26 23:51:24 -0300

    Cria um programa que analisa os ângulos internos de um triângulo

--- fundamentos/introducao-js-logica-de-programacao/dia-1-primeiros-passos/app.js
+++ fundamentos/introducao-js-logica-de-programacao/dia-1-primeiros-passos/app.js
@@ -1,9 +1,13 @@
-const a = 0;
+const anguloA = 40;
+const anguloB = 30;
+const anguloC = 110;
 
-if (a > 0) {
-    console.log('Positive');
-} else if (a < 0) {
-    console.log('Negative');
-} else {
-    console.log('Zero');
+let somaDosAngulos = anguloA + anguloB + anguloC;
+
+if (anguloA <= 0 || anguloB <= 0 || anguloC <= 0) {
+    console.log('error: encontrado valor não positivo para um ou mais ângulos!');
+} else if (somaDosAngulos === 180) {
+    console.log('true');
+} else if (somaDosAngulos !== 180) {
+    console.log('false');
 }
