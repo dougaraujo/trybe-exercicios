13a995d56a8e5767d47c47f933c0e7763c8eec7f
Douglas Araújo <goudxs@gmail.com>
2022-06-27 11:47:39 -0300

    Faz um for/in que mostra todas as chaves do objeto

--- fundamentos/introducao-js-logica-de-programacao/dia-4-objetos-e-funcoes/app.js
+++ fundamentos/introducao-js-logica-de-programacao/dia-4-objetos-e-funcoes/app.js
@@ -1,13 +1,17 @@
-let info = {
+let infos = {
   personagem: "Margarida",
   origem: "Pato Donald",
   nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
 };
 
-console.log("Bem-vinda,", info.personagem);
+console.log("---- Bem-vinda,", infos.personagem, '----');
 
 var newDataName = 'recorrente';
 
-info[newData] = 'Sim';
+infos[newDataName] = 'Sim';
 
-console.log(info);
+console.log(infos);
+
+for ( info in infos ) {
+    console.log(info);
+}
