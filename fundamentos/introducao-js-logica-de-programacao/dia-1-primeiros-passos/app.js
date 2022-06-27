fd415aa8a5b06837b075a1255dc0fe8a55056f23
Douglas Araújo <goudxs@gmail.com>
2022-06-27 00:19:56 -0300

    Escreve um programa que converte percentual de acertos em notas de A a F, e retorna o resultado.

--- fundamentos/introducao-js-logica-de-programacao/dia-1-primeiros-passos/app.js
+++ fundamentos/introducao-js-logica-de-programacao/dia-1-primeiros-passos/app.js
@@ -1,30 +1,25 @@
-let piece = 'peão';
+let porcentagem = 55;
 
-switch (piece.toLowerCase()) {
-    case 'torre':
-        console.log('A torre se move em linha reta horizontalmente e verticalmente.');
-        break;
-    
-    case 'peão':
-        console.log('De maneira geral, os peões se movem somente para frente, uma casa por vez. Um exceção é a primeira vez que um peão é movido, quando ele pode se mover duas casas.');
-        break;
+let nota = "";
 
-    case 'bispo':
-        console.log('O bispo se move em uma linha reta diagonalmente no tabuleiro.');
-        break;
-
-    case 'cavalo':
-        console.log('O cavalo move-se por duas casas horizontalmente ou verticalmente e então uma casa a mais em uma ângulo reto. O movimento do cavalo forma um “L”.');
-        break;
-
-    case 'rainha':
-        console.log('Ela pode mover-se qualquer número de casas em linha reta - verticalmente, horizontalmente ou diagonalmente.');
-        break;
-
-    case 'rei':
-        console.log('O rei pode se mover para qualquer casa adjacente.');
-        break;
-
-    default: 
-        console.log('Insira uma peça do xadrez válida.');
+if (porcentagem < 0 || porcentagem > 100) {
+  console.log("error: valor percentual incorreto.");
+} else if (porcentagem >= 90) {
+  nota = "A";
+  console.log("Sua Nota ==>", nota);
+} else if (porcentagem >= 80) {
+  nota = "B";
+  console.log("Sua Nota ==>", nota);
+} else if (porcentagem >= 70) {
+  nota = "C";
+  console.log("Sua Nota ==>", nota);
+} else if (porcentagem >= 60) {
+  nota = "D";
+  console.log("Sua Nota ==>", nota);
+} else if (porcentagem >= 50) {
+  nota = "E";
+  console.log("Sua Nota ==>", nota);
+} else if (porcentagem < 50) {
+  nota = "F";
+  console.log("Sua Nota ==>", nota);
 }
