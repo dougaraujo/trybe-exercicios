e6a99686f067abed8e1e431430e3f9cbb8b88986
Douglas Araújo <goudxs@gmail.com>
2022-06-27 00:06:42 -0300

    Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.

--- fundamentos/introducao-js-logica-de-programacao/dia-1-primeiros-passos/app.js
+++ fundamentos/introducao-js-logica-de-programacao/dia-1-primeiros-passos/app.js
@@ -1,13 +1,30 @@
-const anguloA = 40;
-const anguloB = 30;
-const anguloC = 110;
+let piece = 'peão';
 
-let somaDosAngulos = anguloA + anguloB + anguloC;
+switch (piece.toLowerCase()) {
+    case 'torre':
+        console.log('A torre se move em linha reta horizontalmente e verticalmente.');
+        break;
+    
+    case 'peão':
+        console.log('De maneira geral, os peões se movem somente para frente, uma casa por vez. Um exceção é a primeira vez que um peão é movido, quando ele pode se mover duas casas.');
+        break;
 
-if (anguloA <= 0 || anguloB <= 0 || anguloC <= 0) {
-    console.log('error: encontrado valor não positivo para um ou mais ângulos!');
-} else if (somaDosAngulos === 180) {
-    console.log('true');
-} else if (somaDosAngulos !== 180) {
-    console.log('false');
+    case 'bispo':
+        console.log('O bispo se move em uma linha reta diagonalmente no tabuleiro.');
+        break;
+
+    case 'cavalo':
+        console.log('O cavalo move-se por duas casas horizontalmente ou verticalmente e então uma casa a mais em uma ângulo reto. O movimento do cavalo forma um “L”.');
+        break;
+
+    case 'rainha':
+        console.log('Ela pode mover-se qualquer número de casas em linha reta - verticalmente, horizontalmente ou diagonalmente.');
+        break;
+
+    case 'rei':
+        console.log('O rei pode se mover para qualquer casa adjacente.');
+        break;
+
+    default: 
+        console.log('Insira uma peça do xadrez válida.');
 }
