f38137668902c4b61f6620109a6eb78c617a671c
Douglas Araújo <goudxs@gmail.com>
2022-06-27 11:45:32 -0300

    Insere uma nova propriedade ao objeto e imprime as infos na tela

--- fundamentos/introducao-js-logica-de-programacao/dia-4-objetos-e-funcoes/app.js
+++ fundamentos/introducao-js-logica-de-programacao/dia-4-objetos-e-funcoes/app.js
@@ -4,4 +4,10 @@ let info = {
   nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
 };
 
-console.log('Bem-vinda,', info.personagem);
+console.log("Bem-vinda,", info.personagem);
+
+var newDataName = 'recorrente';
+
+info[newData] = 'Sim';
+
+console.log(info);
