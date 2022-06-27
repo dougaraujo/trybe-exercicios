e42936c2698dba979fae0383df8f61bb6513939e
Douglas Araújo <goudxs@gmail.com>
2022-06-27 12:02:00 -0300

    Define um novo objeto e imprime suas propriedade e valores

--- fundamentos/introducao-js-logica-de-programacao/dia-4-objetos-e-funcoes/app.js
+++ fundamentos/introducao-js-logica-de-programacao/dia-4-objetos-e-funcoes/app.js
@@ -4,18 +4,27 @@ let infos = {
   nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
 };
 
-console.log("---- Bem-vinda,", infos.personagem, '----');
+console.log("---- Bem-vinda,", infos.personagem, "----");
 
-var newDataName = 'recorrente';
+var newDataName = "recorrente";
 
-infos[newDataName] = 'Sim';
+infos[newDataName] = "Sim";
 
 console.log(infos);
 
-for ( info in infos ) {
-    console.log(info);
+for (info in infos) {
+  console.log(info);
 }
 
 for (info in infos) {
-    console.log(infos[info]);
+  console.log(infos[info]);
 }
+
+infos2 = Object.assign(infos);
+
+infos2.personagem = "Tio Patinhas";
+infos2.origem = "Christmas on Ber Mountain, Dell's Four Color Comics \#178\ .";
+infos2.nota = "O último MacPatinhas";
+infos2.recorrente = "Sim";
+
+console.log(infos2);
