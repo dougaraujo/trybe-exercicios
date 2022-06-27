3b445dfc286aaeb83f48ffdbcbe144cd7c64c954
Douglas Araújo <goudxs@gmail.com>
2022-06-27 12:19:18 -0300

    Imprime as infos do objeto leitor e adiciona um novo livro favorito

--- fundamentos/introducao-js-logica-de-programacao/dia-4-objetos-e-funcoes/app.js
+++ fundamentos/introducao-js-logica-de-programacao/dia-4-objetos-e-funcoes/app.js
@@ -1,4 +1,4 @@
-let infos = {
+/*let infos = {
   personagem: "Margarida",
   origem: "Pato Donald",
   nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
@@ -33,6 +33,35 @@ for (key in infos) {
   } else {
     console.log(infos[key], "e", infos2[key]);
   }
-}
+}*/
 
+let leitor = {
+  nome: "Julia",
+  sobrenome: "Pessoa",
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: "'O Pior Dia de Todos'",
+      autor: "Daniela Kopsch",
+      editora: "Tordesilhas",
+    },
+  ],
+};
 
+console.log(
+  "O livro favorito de",
+  leitor.nome,
+  leitor.sobrenome,
+  "se chama",
+  leitor.livrosFavoritos[0].titulo
+);
+
+let newFavoriteBook = {
+  titulo: "Harry Potter e o Prisioneiro de Azkaban",
+  autor: "JK Rowling",
+  editora: "Rocco",
+};
+
+leitor.livrosFavoritos.push(newFavoriteBook);
+
+console.log(leitor);
